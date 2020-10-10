2c5d983594236ee984e791f0eb1c2709b31d774b
Hans Bricks <33793809+gyrusdentatus@users.noreply.github.com>
2020-10-10 20:55:45 +0200

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#set -x
+
+
+# This function checks the version and if the file exists.
+# If it does not exist or it is not the latest version, it downloads the latest release
+# from the official github repo.
+function downloader () {
+
+# set vars for version checking and url to download the latest release of nym-mixnode
+VERSION=$(curl https://github.com/nymtech/nym/releases/latest --cacert /etc/ssl/certs/ca-certificates.crt 2>/dev/null | egrep -o "[0-9|\.]{5}(-\w+)?")
+URL="https://github.com/nymtech/nym/releases/download/v$VERSION/nym-mixnode_linux_x86_64"
+
+# Check if the version is up to date. If not, fetch the latest release.
+if [ ! -f nym-mixnode_linux_x86_64 ] || [ "$(./nym-mixnode_linux_x86_64 --version | grep Nym | cut -c 13- )" != "$VERSION" ]
+then
+   curl -L -s "$URL" -o "nym-mixnode_linux_x86_64" --cacert /etc/ssl/certs/ca-certificates.crt && echo "Fetching the latest version"
+# Make it executable
+   chmod +x ./nym-mixnode_linux_x86_64
+else
+   echo "You have the latest version of Nym-mixnode $VERSION"
+fi
+}
+
+# set vars for the init phase
+ip=$(curl -s ipinfo.io/ip)
+location=$(curl -s ipinfo.io/city)
+ID="nym"
+layer=$(echo $((1 + RANDOM % 3)))
+
+# Check if the config files and keys exist and if not then it creates them with id "nym" locate in user home folder at .nym/
+function config_exists () {
+if [ ! -f $HOME/.nym/mixnodes/$ID/data/private_sphinx.pem ] || [ ! -f $HOME/.nym/mixnodes/$ID/data/public_sphinx.pem ]; then
+    echo "Missing pem files, running init"
+    ./nym-mixnode_linux_x86_64 init --host 0.0.0.0 --announce-host ${ip} --id ${ID} --layer ${layer} --location ${location}
+elif [ -f $HOME/.nym/mixnodes/${ID}/data/private_sphinx.pem ] || [ -f $HOME/.nym/mixnodes/${ID}/data/public_sphinx.pem ]; then
+    echo "Keys and config files found. Not overwriting ..."
+fi
+}
+downloader
+config_exists
